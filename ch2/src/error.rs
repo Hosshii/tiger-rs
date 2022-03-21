4e0e53e655b6e8e0ae428b306135aa7cada63466
Hosshii <sao_heath6147.wistre@icloud.com>
2022-03-21 15:55:09 +0900

    lexer

---
+++ ch2/src/error.rs
@@ -0,0 +1,52 @@
+use std::{fmt, io};
+
+use thiserror::Error;
+
+use crate::position::Meta;
+
+#[derive(Debug, Error)]
+pub struct Error {
+    pub(crate) kind: ErrorKind,
+    pub(crate) meta: Meta,
+}
+
+impl Error {
+    pub fn new(kind: ErrorKind, meta: Meta) -> Self {
+        Self { kind, meta }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(
+            f,
+            "file: {}, line: {}, column: {}",
+            self.meta.filename(),
+            self.meta.line(),
+            self.meta.column()
+        )?;
+        write!(f, "{}", self.kind)
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ErrorKind {
+    #[error("reached end of file")]
+    Eof,
+
+    // expected, actual
+    #[error("unexpected character. expected: `{}`, found: `{}`.",*.0 as char, *.1 as char)]
+    UnexpectedCharacter(u8, u8),
+
+    #[error("invalid escape.")]
+    InvalidEscape,
+
+    #[error(transparent)]
+    Io(#[from] io::Error),
+
+    #[error(transparent)]
+    Utf8(#[from] std::str::Utf8Error),
+
+    #[error("unknown error {0}")]
+    Other(String),
+}
--- ch2/src/main.rs
+++ ch2/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs::File};
+
+use ch2::Lexer;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let filename = env::args().nth(1).expect("expect filename");
+    let file = File::open(filename.as_str())?;
+
+    let mut lexer = Lexer::new(filename, file);
+    let tokens = match lexer.tokenize() {
+        Ok(token) => token,
+        Err(e) => {
+            println!("{}", &e);
+            return Err(e.into());
+        }
+    };
+
+    tokens
+        .into_iter()
+        .for_each(|t| print!("{} ", t.kind.to_string()));
+
+    Ok(())
 }
