d4068c0c9bdab1726b13e820b2c3fe5ae7e6374b
Hosshii <sao_heath6147.wistre@icloud.com>
2022-06-15 16:33:49 +0900

    Define ir tree and unwrap_ex/nx/cx function

---
+++ tiger/src/ir.rs
@@ -0,0 +1,67 @@
+use crate::temp::{Label, Temp};
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Expr {
+    Const(i64),
+    Name(Label),
+    Temp(Temp),
+    BinOp(BinOp, Box<Expr>, Box<Expr>),
+    Mem(Box<Expr>, u64),
+    Call(Box<Expr>, Vec<Expr>),
+    // maybe unnecessary box for stmt
+    ESeq(Box<Stmt>, Box<Expr>),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Stmt {
+    Move(Box<Expr>, Box<Expr>),
+    Expr(Box<Expr>),
+    Jump(Box<Expr>, Vec<Label>),
+    CJump(RelOp, Box<Expr>, Box<Expr>, Label, Label),
+    Seq(Box<Stmt>, Box<Stmt>),
+    Label(Label),
+}
+
+impl Stmt {
+    pub fn seq(first: Stmt, second: Stmt, mut remain: Vec<Stmt>) -> Stmt {
+        let mut v = Vec::with_capacity(remain.len() + 2);
+        v.push(first);
+        v.push(second);
+        v.append(&mut remain);
+        v.into_iter()
+            .reduce(|accum, item| {
+                let lhs = Box::new(accum);
+                let rhs = Box::new(item);
+                Stmt::Seq(lhs, rhs)
+            })
+            .unwrap()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum BinOp {
+    Plus,
+    Minus,
+    Mul,
+    Div,
+    And,
+    Or,
+    LShift,
+    RShift,
+    ARShift,
+    XOr,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum RelOp {
+    Eq,
+    Ne,
+    Lt,
+    Gt,
+    Le,
+    Ge,
+    Ult,
+    Ule,
+    Ugt,
+    Uge,
+}
--- tiger/src/translate.rs
+++ tiger/src/translate.rs
@@ -1,8 +1,9 @@
-use std::{marker::PhantomData, sync::atomic::AtomicU32};
+use std::{fmt::Debug, marker::PhantomData, sync::atomic::AtomicU32};
 
 use crate::{
     frame::{Frame, X86 as X86Frame},
-    temp::Label,
+    ir::{Expr as IrExpr, RelOp, Stmt},
+    temp::{Label, Temp},
 };
 
 pub trait Translate {
@@ -16,12 +17,88 @@ pub trait Translate {
     fn alloc_local(level: Self::Level, is_escape: bool) -> Self::Access;
 }
 
-#[derive(Debug)]
-pub struct Expr {}
+type CxFn<'a> = dyn (FnOnce(Label, Label) -> Stmt) + 'a;
+
+pub enum Expr {
+    Ex(IrExpr),
+    Nx(Stmt),
+    Cx(Box<CxFn<'static>>),
+}
+
+impl Expr {
+    fn unwrap_ex(self) -> IrExpr {
+        match self {
+            Expr::Ex(e) => e,
+            Expr::Nx(stmt) => IrExpr::ESeq(Box::new(stmt), Box::new(IrExpr::Const(0))),
+            Expr::Cx(cx_fn) => {
+                let r = Temp::new();
+                let t = Label::new();
+                let f = Label::new();
+                IrExpr::ESeq(
+                    Box::new(Stmt::seq(
+                        Stmt::Move(
+                            Box::new(IrExpr::Temp(r.clone())),
+                            Box::new(IrExpr::Const(1)),
+                        ),
+                        cx_fn(t.clone(), f.clone()),
+                        vec![
+                            Stmt::Label(f),
+                            Stmt::Move(
+                                Box::new(IrExpr::Temp(r.clone())),
+                                Box::new(IrExpr::Const(0)),
+                            ),
+                            Stmt::Label(t),
+                        ],
+                    )),
+                    Box::new(IrExpr::Temp(r)),
+                )
+            }
+        }
+    }
+
+    fn unwrap_nx(self) -> Stmt {
+        match self {
+            Expr::Ex(e) => Stmt::Expr(Box::new(e)),
+            Expr::Nx(s) => s,
+            Expr::Cx(cx_fn) => {
+                let t = Label::new();
+                let f = Label::new();
+                cx_fn(t, f)
+            }
+        }
+    }
+
+    fn unwrap_cx(self) -> Box<CxFn<'static>> {
+        match self {
+            Expr::Ex(e) => {
+                let f = |t: Label, f: Label| {
+                    // if e == 0 then goto t
+                    // else goto f
+                    Stmt::CJump(RelOp::Eq, Box::new(e), Box::new(IrExpr::Const(0)), t, f)
+                };
+
+                Box::new(f)
+            }
+            Expr::Nx(_) => unreachable!(),
+            Expr::Cx(f) => f,
+        }
+    }
+}
 
 impl Expr {
     pub fn new() -> Self {
-        Self {}
+        todo!()
+        // Self {}
+    }
+}
+
+impl Debug for Expr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Expr::Ex(ir) => ir.fmt(f),
+            Expr::Nx(stm) => stm.fmt(f),
+            Expr::Cx(_) => write!(f, "closure"),
+        }
     }
 }
 
