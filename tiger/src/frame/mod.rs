7c7c698f6f33ef075a2c90b13b7f9fe59cfaade5
Hosshii <sao_heath6147.wistre@icloud.com>
2022-06-15 22:36:42 +0900

    Add explanation to Frame trait.

--- tiger/src/frame/mod.rs
+++ tiger/src/frame/mod.rs
@@ -7,16 +7,30 @@ use crate::{
 pub use x86::X86;
 
 pub trait Frame: Clone {
+    /// Represents an access to variable.
+    /// Typically implemented like `enum {InReg(Temp), InFrame(offset)}`.
     type Access: Clone;
 
+    /// Machine specific word size.
     const WORD_SIZE: u64;
 
+    /// Represents frame pointer.
     fn fp() -> Temp;
 
+    /// Create new frame with given name and the escape information of arguments.
     fn new(name: Label, formals: Vec<bool>) -> Self;
+
+    /// Return frame name
     fn name(&self) -> &Label;
+
+    /// Return `Access` for formals.
     fn formals(&self) -> &[Self::Access];
+
+    /// Allocate new local variable with given escape information.
+    /// Returns the `Access` to the created variable.
     fn alloc_local(&mut self, is_escape: bool) -> Self::Access;
 
+    /// Convert an `Access` to IR Tree.
+    /// The second argument means stack address in which `Access` is allocated.
     fn exp(access: Self::Access, stack_addr: Expr) -> Expr;
 }
