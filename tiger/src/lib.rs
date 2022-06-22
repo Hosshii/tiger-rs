2ad49f5b7781626d475474a4d0654f7a515b3326
Hosshii <sao_heath6147.wistre@icloud.com>
2022-06-22 18:20:09 +0900

    Change module visibility

--- tiger/src/lib.rs
+++ tiger/src/lib.rs
@@ -1,13 +1,13 @@
 pub mod lexer;
 pub mod parser;
-
-pub mod env;
-pub mod escape;
-pub mod frame;
-pub mod ir;
-mod position;
 pub mod semant;
-pub mod symbol;
-pub mod temp;
-pub mod translate;
-pub mod types;
+
+mod env;
+mod escape;
+mod frame;
+mod ir;
+mod position;
+mod symbol;
+mod temp;
+mod translate;
+mod types;
