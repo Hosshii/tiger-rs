0b94f3edca32a5f8cace526d26a93f6ff1b156b2
Hosshii <sao_heath6147.wistre@icloud.com>
2022-07-14 21:00:00 +0900

    Add bin test

---
+++ bin_test/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# set -eux
+set -u
+
+assert() {
+    local expected="$1"
+    local file="./bin_test/$2"
+
+    local bin="./target/release/tiger"
+
+    $bin "$file" > tmp.s
+    cc -o tmp tmp.s 
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$2: success!!"
+    else
+        echo "$file => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+assert 1 test1.tig
