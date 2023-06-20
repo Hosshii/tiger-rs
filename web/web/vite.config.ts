e8f2963b43362f11242ad7a757d1645c5804350f
Hosshii <sao_heath6147.wistre@icloud.com>
2023-06-21 03:17:08 +0900

    update

--- web/web/vite.config.ts
+++ web/web/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES // この行を追加
+    ? "tiger-rs" // この行を追加
+    : "",
   plugins: [react()],
-})
+});
